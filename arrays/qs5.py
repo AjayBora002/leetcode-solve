440d37df9b1cd1de874011d018c5276141aadbc0
Ajay <boraajay26@gmail.com>
2026-09-07 09:14:14 +0530

    new changes

--- arrays/qs5.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-	def fib(self, n: int) -> int:
-		# Defined INSIDE fib, so it doesn't need 'self'
-		def func(num):
-			if num == 0 or num == 1:
-				return num
-			return func(num - 1) + func(num - 2)
-		
-		return func(n)
